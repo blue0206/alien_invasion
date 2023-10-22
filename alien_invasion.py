198d4c191789a25128767c3864720012a2e2df21
Aayush Rai <coolaayushrai@gmail.com>
2023-10-22 12:37:24 +0530

    Import settings module.
    Import Settings Class from module
    containing all game settings.
    Set display size, background color,
    fps.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
-    """Overall class to manage game assets and behaviour."""
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1920, 1080))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
     
     def run_game(self):
@@ -19,9 +24,14 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type() == pygame.QUIT:
                     sys.exit()
+
+            #Redraw the screen during each pass.
+            self.screen.fill(self.settings.bg_color)
             
             #Make the most recently drawn screen visible.
             pygame.display.flip()
+            #Set fps to 60 by making the loop run exactly 60 times/sec.
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
