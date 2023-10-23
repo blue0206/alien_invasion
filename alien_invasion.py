6a8dca6f9516a7ad4afc3d1edd2c54e7a60ba26d
Aayush Rai <coolaayushrai@gmail.com>
2023-10-23 23:42:48 +0530

    Import ship class
    Invoke Ship class and sync
    display background with ship
    photo. Use rect for alignment.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -16,18 +17,21 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                                self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
     
     def run_game(self):
         """Start the main loop for the game."""
-        while True:
             #Watch for keyboard and mouse events.
+        while True:
             for event in pygame.event.get():
                 if event.type() == pygame.QUIT:
                     sys.exit()
 
             #Redraw the screen during each pass.
             self.screen.fill(self.settings.bg_color)
-            
+            self.ship.blitme()
+
             #Make the most recently drawn screen visible.
             pygame.display.flip()
             #Set fps to 60 by making the loop run exactly 60 times/sec.
